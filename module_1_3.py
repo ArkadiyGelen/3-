1ed53db862705b43d7281d25382856c000871a3d
ArkadiyGelen <ArkadijTolstov@gmail.com>
2024-09-04 00:28:51 +0300

    Update module_1_3.py

--- module_1_3.py
+++ module_1_3.py
@@ -2,6 +2,7 @@ name = "Atkadiy"
 print("Name:", name)
 age = 35
 print("Age:", age)
-new_age = 36
-print("New age:", new_age)
-print("Is student:", age < new_age)
+age = age +1
+print("New age:", age)
+is_student = True
+print("Is student:", is_student)
